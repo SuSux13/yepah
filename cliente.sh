87d891c88db9a6cf2b8f3d2a084590fb550b73ce
SuSux13 <55140831+SuSux13@users.noreply.github.com>
2020-03-06 13:24:34 +0100

    Update cliente.sh

--- cliente.sh
+++ cliente.sh
@@ -1,14 +1,7 @@
+cd $HOME
 git clone https://github.com/SuSux13/comandosTERMUX
-echo "bash comandos.sh" > quepasa.sh
-ccryrpt -e quepasa.sh
-ccryrpt -d quepasa.sh
 cd comandosTERMUX
-mkdir hehe
-cd hehe
-mv comandos.sh /data/data/com.termux/files/home/hehe/ver
-mv banner.txt /data/data/com.termux/files/home/hehe/ver
-cd ..
-bash quepasa.sh
+bash comandos.sh
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #import location_pb2
